a3b831faa4102cbcc1ccdb432a59ce486753b9e2
R N Dhanapraveenkrishna <rndpk12@gmail.com>
2026-06-09 15:13:41 +0530

    Add Vercel URL to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173"
+                   "https://quant-lab-tau.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
